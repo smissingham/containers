62bf515698a8cdcacf4dbb5a1ef58aa0065ea7da
Sean Missingham <sean@missingham.com>
2025-04-23 09:23:20 -0500

    Init Commit

---
+++ _run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+podman-compose -f compose.core.yml up -d --remove-orphans
+podman-compose -f compose.home.yml up -d --remove-orphans
+podman-compose -f compose.llm.yml up -d --remove-orphans
+#podman-compose -f llm.docker-compose.yml up -d --remove-orphans
+#podman-compose -f games.docker-compose.yml up -d --remove-orphans
+
+
+
+# Load environment variables
+#if [ -f .env ]; then
+#    export $(grep -v '^#' .env | xargs)
+#fi
+#sudo chown -R $PUID:$PGID $APP_DATA
