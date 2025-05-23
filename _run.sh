0241683a1d0535367ee7425fc0d580c5220557df
Sean Missingham <sean@missingham.com>
2025-05-23 10:54:24 -0500

    Updates

--- _run.sh
+++ _run.sh
@@ -4,6 +4,7 @@
 podman-compose -f compose.core.yml up -d --remove-orphans
 podman-compose -f compose.home.yml up -d --remove-orphans
 podman-compose -f compose.llm.yml up -d --remove-orphans
+podman-compose -f compose.media.yml up -d --remove-orphans
 #podman-compose -f llm.docker-compose.yml up -d --remove-orphans
 #podman-compose -f games.docker-compose.yml up -d --remove-orphans
 
